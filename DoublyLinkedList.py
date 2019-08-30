6e9b704c827365ee5cebe487cfa688d33c5bea2a
Fanzil <fan.hakimov2010@yandex.ru>
2019-08-30 21:55:11 +0300

    updated some methods, added __add__ method, corrected docs

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -1,6 +1,6 @@
 class Node:
     """
-    Linked list element class.
+    A linked list element class.
     """
     def __init__(self, value=None, prev=None, next=None):
         self.val = value
@@ -10,9 +10,9 @@ class Node:
 
 class DLinkedList:
     """
-    Linked list class, whose elements are an instance of the Node class.
-    :param root: 1st element of Linked List
-    :param end: last element of Linked List
+    A linked list class, whose elements are an instance of the Node class.
+    :param root: The 1st element
+    :param end: The last element
     """
     def __init__(self, *values):
         self.root = None
@@ -21,90 +21,62 @@ class DLinkedList:
         for i in values:
             self.addAtTail(i)
 
-    def _get(self, index):
+    def addAtHead(self, *values):
         """
-        Get the index-th node in the linked list.
-        """
-        if index >= len(self):
-            raise IndexError
-        if index == 0:
-            return self.root
-        if index == len(self) - 1:
-            return self.end
-        if index < 0:
-            if abs(index) > len(self):
-                raise IndexError
-            index += len(self)
-        temp = None
-        if index <= len(self) // 2:
-            temp = self.root
-            for i in range(index):
-                temp = temp.next
-        else:
-            temp = self.end
-            for i in range(len(self) - index - 1):
-                temp = temp.prev
-        return temp
-
-    def addAtHead(self, val, *values):
-        """
-        Add a node(s) of value(s) val(values) before the first element of the linked list.
-        :param val: node value
-        :param values: additional values
+        Add a node(s) with value(s) before the first element of a linked list.
+        :param values: a number or a sequence of numbers
         :return:
         """
-        for i in values[::-1]:
-            self.addAtHead(i)
-        if not self.root:
-            self.root = Node(val)
-            self.end = self.root
-        else:
+        for val in values[::-1]:
             node = Node(val, None, self.root)
-            self.root.prev = node
+            try:
+                self.root.prev = node
+            except AttributeError:
+                pass
             self.root = node
-        self._length += 1
+            if not self.root.next:
+                self.end = self.root
+        self._length += len(values)
 
-    def addAtTail(self, val, *values):
+    def addAtTail(self, *values):
         """
-        Append a node(s) of value(s) val(values) to the last element of the linked list.
-        :param val: node value
+        Append a node(s) with value(s) to the last element of a linked list.
         :param values: additional values
         :return:
         """
         if not self.end:
-            self.addAtHead(val)
+            self.addAtHead(*values)
         else:
-            self.end.next = Node(val, self.end)
-            self.end = self.end.next
-            self._length += 1
-        for i in values:
-            self.addAtTail(i)
+            for val in values:
+                self.end.next = Node(val, self.end)
+                self.end = self.end.next
+            self._length += len(values)
 
-    def addAtIndex(self, index, val, *values):
+    def addAtIndex(self, index, *values):
         """
-        Add a node(s) of value(s) val(values) before the index-th node in the linked list. If index equals to the length
-        of linked list, the node will be appended to the end of linked list.
-        :param index: node index
-        :param val: node value
-        :param values: additional values
+        Add a node(s) with value(s) before the index-th node in a linked list. If the index equals to the length
+        of the linked list, the node will be appended to the end of the linked list.
+        :param index: the node index
+        :param values: a number or a sequence of numbers
         :return:
         """
         if index == 0:
-            self.addAtHead(val, *values)
+            self.addAtHead(*values)
         elif index == len(self):
-            self.addAtTail(val, *values)
+            self.addAtTail(*values)
         elif 0 < index < len(self):
-            temp = self._get(index - 1)
-            node = Node(val, temp, temp.next)
-            temp.next = node
-            node.next.prev = node
-            self._length += 1
+            for val in values:
+                temp = self._get(index - 1)
+                node = Node(val, temp, temp.next)
+                temp.next = node
+                node.next.prev = node
+            self._length += len(values)
         else:
             raise IndexError
 
     def deleteAtIndex(self, index):
         """
-        Delete the index-th node in the linked list, if the index is valid.
+        Delete the index-th node in a linked list, if the index is valid.
         :param index: node index
         :return:
         """
@@ -128,8 +100,8 @@ class DLinkedList:
 
     def popRoot(self):
         """
-        Remove the first node in the linked list and return its value.
-        :return value: 1st node value
+        Remove the first node from a linked list and return its value.
+        :return value: The 1st node value
         """
         if len(self) == 0:
             return None
@@ -139,14 +111,39 @@ class DLinkedList:
 
     def popEnd(self):
         """
-        Remove the last node in the linked list and return its value.
-        :return value: last node value
+        Remove the last node from a linked list and return its value.
+        :return value: The last node value
         """
         if len(self) < 2:
             return self.popRoot()
         res = self.end.val
         self.deleteAtIndex(len(self) - 1)
         return res
+
+    def _get(self, index):
+        """
+        Get the index-th node in a linked list.
+        """
+        if index >= len(self):
+            raise IndexError
+        if index == 0:
+            return self.root
+        if index == len(self) - 1:
+            return self.end
+        if index < 0:
+            if abs(index) > len(self):
+                raise IndexError
+            index += len(self)
+        temp = None
+        if index <= len(self) // 2:
+            temp = self.root
+            for i in range(index):
+                temp = temp.next
+        else:
+            temp = self.end
+            for i in range(len(self) - index - 1):
+                temp = temp.prev
+        return temp
 
     def __len__(self):
         return self._length
@@ -179,5 +176,12 @@ class DLinkedList:
             temp = temp.next
         return temp is not None
 
+    def __add__(self, other):
+        first = [i for i in self]
+        second = [i for i in other]
+        result = DLinkedList(*first)
+        result.addAtTail(*second)
+        return result
+
     def __str__(self):
-        return ' -- '.join(str(i) for i in self)
+        return ' >< '.join(str(i) for i in self)
