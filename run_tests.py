5b45b01524baaa1b20cd3eb54fa155617d12faf9
Fanzil <fan.hakimov2010@yandex.ru>
2019-10-22 22:49:53 +0600

    add tests running script

---
+++ run_tests.py
@@ -0,0 +1,6 @@
+from Tests.test_singly_linked_list import TestList
+from Tests.test_doubly_linked_list import TestDList
+
+if __name__ == "__main__":
+    TestList.run_test()
+    TestDList.run_test()
